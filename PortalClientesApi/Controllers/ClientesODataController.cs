3960ef5df3e7c7944b6f77b4edbdcc89880550c4
luleiacom <liacono50@gmail.com>
2026-09-16 18:03:42 -0300

    Agrega endpoint OData para consultas avanzadas sobre Clientes

---
+++ PortalClientesApi/Controllers/ClientesODataController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+using PortalClientesApi.Data;
+using PortalClientesApi.Models;
+
+namespace PortalClientesApi.Controllers
+{
+    public class ClientesODataController : ODataController
+    {
+        private readonly AppDbContext _context;
+
+        public ClientesODataController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [EnableQuery]
+        [HttpGet("odata/ClientesOData")]
+        public IQueryable<Cliente> Get()
+        {
+            return _context.Clientes;
+        }
+    }
+}
--- PortalClientesApi/Program.cs
+++ PortalClientesApi/Program.cs
@@ -1,9 +1,18 @@
 using PortalClientesApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+using PortalClientesApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+var modelBuilder = new ODataConventionModelBuilder();
+modelBuilder.EntitySet<Cliente>("ClientesOData");
+
+builder.Services.AddControllers().AddOData(options =>
+    options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(100)
+        .AddRouteComponents("odata", modelBuilder.GetEdmModel()));
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
